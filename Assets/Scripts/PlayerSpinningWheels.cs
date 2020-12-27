6411978c662ea07ba514b49d1bad27505a001fae
Bodzio Podolak, Jr <bodzio2k@live.com>
2020-12-27 13:17:06 +0100

    Kółka wozu się teraz kręcą.

---
+++ Assets/Scripts/PlayerSpinningWheels.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpinningWheels : MonoBehaviour
+{
+    public GameObject frontWheel, rearWheel;
+    [Range(360.0f, 1080.0f)]
+    public float rotationSpeed = 360.0f;
+
+    void Update()
+    {
+        Vector3 rotation = new Vector3(0, 0, -rotationSpeed * Time.deltaTime);
+        frontWheel.transform.Rotate(rotation);
+        rearWheel.transform.Rotate(rotation);
+    }
+}
