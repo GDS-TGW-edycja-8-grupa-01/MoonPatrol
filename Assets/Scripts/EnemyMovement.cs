c8c6f0c235d088bd0a5ea1ff37d3dfae89a7a773
Bodzio Podolak, Jr <bodzio2k@live.com>
2020-12-31 12:14:23 +0100

    Nowe tło Gosi już się paralaksi...

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float range;
+    public float magnitude;
+    public float speed;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Vector2 origin;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+
+        origin = transform.position;
+
+        return;
+    }
+
+    void Update()
+    {
+        Vector3 direction;
+
+        if (transform.position.x >= origin.x + range)
+        {
+            direction = Vector3.left;
+        }
+        else
+        {
+            direction = Vector3.right;
+        }
+
+        transform.position += direction * Time.deltaTime * speed;
+    }
+}
