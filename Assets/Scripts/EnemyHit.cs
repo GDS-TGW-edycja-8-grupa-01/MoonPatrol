56bd2b5deb391e09d7247d0d32d63f4b1fcc7302
Bodzio Podolak, Jr <bodzio2k@live.com>
2021-01-02 11:50:20 +0100

    Wrogie latające spodki można teraz rozwalać.

---
+++ Assets/Scripts/EnemyHit.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Reflection;
+
+public class EnemyHit : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject explosion;
+
+    private Animator a;
+    private bool animatorExists = false;
+
+    void Start()
+    {
+        explosion = transform.GetChild(0).gameObject;
+
+        animatorExists = explosion.TryGetComponent<Animator>(out a);
+
+        if (animatorExists) a.enabled = false;
+
+        explosion.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+
+    private void OnDestroy()
+    {
+        Debug.LogFormat("{0} Destroyed...", MethodBase.GetCurrentMethod());
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (animatorExists)
+        {
+            float delay = a.GetCurrentAnimatorClipInfo(0).Length;
+
+            a.enabled = true;
+            GetComponent<SpriteRenderer>().enabled = false;
+            a.Play("Base Layer.Explosion");
+
+            Destroy(this.gameObject, delay);
+            Destroy(collision.gameObject);
+        }
+    }
+}
