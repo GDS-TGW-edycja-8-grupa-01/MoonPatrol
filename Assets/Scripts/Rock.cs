20cb09c0f2c8ed4ffc44b642ba434a8578818816
Bodzio Podolak, Jr <bodzio2k@live.com>
2021-01-06 18:50:31 +0100

    Skały rozwalają się teraz w drobny mak po przypiepsrzeniu w nie pociskiem.

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    private Animator a;
+    private bool animatorExists = false;
+    private GameObject explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        explosion = transform.Find("Explosion").gameObject;
+
+        animatorExists = explosion.TryGetComponent<Animator>(out a);
+
+        if (animatorExists) a.enabled = false;
+
+        explosion.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (animatorExists)
+        {
+            float delay = a.GetCurrentAnimatorClipInfo(0).Length;
+
+            a.enabled = true;
+            a.Play("Base Layer.Explosion");
+
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<PolygonCollider2D>().enabled = false;
+            
+            Destroy(this.gameObject, delay);
+            Destroy(collision.gameObject);
+        }
+    }
+}
+
