1553a8c06ead8fdd6077a4fa16e15067058e2a3b
Bodzio Podolak, Jr <bodzio2k@live.com>
2021-01-07 17:48:06 +0100

    Korekcja kolizji skał z ziemią.

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -28,7 +28,7 @@ public class Rock : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Ground"))
         {
             return;
         }
