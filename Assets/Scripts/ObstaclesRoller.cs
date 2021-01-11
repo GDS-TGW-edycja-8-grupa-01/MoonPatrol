512ee5b7704f69b6da2449d5526336a77a535638
Bodzio Podolak, Jr <bodzio2k@live.com>
2021-01-11 22:41:13 +0100

    Segregcja wedle osi Z i przygotowanie rollera leveli.

---
+++ Assets/Scripts/ObstaclesRoller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExtensionMethods;
+using System.Reflection;
+
+public class ObstaclesRoller : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject[] levels;
+
+    private Vector3 bounds;
+    private Renderer r;
+
+    void Start()
+    {
+        bounds = this.GetScreenBounds();
+
+        LayoutLevels();
+
+        return;
+    }
+
+    private void LayoutLevels()
+    {
+        Vector3 initialPosition = new Vector3(20.0f, -4.18f, -2.0f);
+        float previousLevelLength = 50.0f;
+
+        for (int i = 0; i < levels.GetLength(0); i++)
+        {
+            Vector3 position = i == 0 ? initialPosition : new Vector3(previousLevelLength, -4.18f, -2.0f);
+
+            Instantiate(levels[i]);
+
+            levels[i].transform.position = position;
+        }
+
+        return;
+    }
+}
