d8b82549a2aaab88c8266bec967eda5d014bd375
jzataj <jakub.zataj@gmail.com>
2021-01-14 00:38:07 +0100

    Skrypt który w fikuśny sposób obraca obiekty (może się przydać w przyszłości) i zmiany folderowe.
    Rozmawialiśmy z Damianem o animacji trzeciego przeciwnika w unity i pokazałem mu przykładowo co jestem w stanie zrobić - powstał z tego funkcjonalny skrypt.

---
+++ Assets/Scripts/ObjectRotator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotator : MonoBehaviour
+{
+    public float anglePerSec = 120.0f;
+    [Range(1.0f, 5.0f)]
+    public float modSpeed = 3.0f;
+    private float rotationMod;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotationMod = Mathf.Sin(Mathf.Sin(Time.time * modSpeed)*Mathf.PI);
+        rotationMod = 1;
+        this.transform.Rotate(0, 0, anglePerSec * Time.deltaTime * rotationMod, Space.Self);
+    }
+}
+
