168726f7c29359127cebfc2b5afa4755e0e0a9f8
Bodzio Podolak, Jr <bodzio2k@live.com>
2021-01-16 13:49:10 +0100

    Kładzenie leveli się zgadza.

--- Assets/Scripts/ObstaclesRoller.cs
+++ Assets/Scripts/ObstaclesRoller.cs
@@ -12,6 +12,9 @@ public class ObstaclesRoller : MonoBehaviour
     private Vector3 bounds;
     private Renderer r;
 
+    [Range(0.0f, 20.0f)]
+    public float offsetX = 1.0f;
+
     void Start()
     {
         bounds = this.GetScreenBounds();
@@ -23,12 +26,24 @@ public class ObstaclesRoller : MonoBehaviour
 
     private void LayoutLevels()
     {
-        Vector3 initialPosition = new Vector3(20.0f, -4.18f, -2.0f);
-        float previousLevelLength = 50.0f;
+        float y = -4.18f;
+        float z = -2.0f;
+        float startingX = 10.0f;
+        Vector3 initialPosition = new Vector3(startingX, y, z);
+        float previousLevelLength = GetLevelWidth(levels[0]);
+        Vector3 position;
 
         for (int i = 0; i < levels.GetLength(0); i++)
         {
-            Vector3 position = i == 0 ? initialPosition : new Vector3(previousLevelLength, -4.18f, -2.0f);
+            if (i == 0)
+            {
+                position = initialPosition;
+            }
+            else
+            {
+                position = new Vector3(startingX + previousLevelLength + offsetX, y, z);
+                previousLevelLength += GetLevelWidth(levels[i]);
+            }
 
             Instantiate(levels[i], transform, true);
 
@@ -36,5 +51,14 @@ public class ObstaclesRoller : MonoBehaviour
         }
 
         return;
+    }
+
+    private float GetLevelWidth(GameObject go)
+    {
+        BoxCollider2D bc = go.GetComponent<BoxCollider2D>();
+        Vector2 size = bc.size;
+        float width = size.x;
+
+        return width;
     }
 }
