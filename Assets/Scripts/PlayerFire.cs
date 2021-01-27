90a173653f3588be5c78db939980dccb6e17d30f
jzataj <jakub.zataj@gmail.com>
2021-01-27 18:08:45 +0100

    Dodany lekki cooldown na strzale gracza do góry
    Liczony jest co 4 strzały, ale jeżeli w ciągu sekundy wystrzelimy mniej niż 4, to w ogóle się nie załączy.

--- Assets/Scripts/PlayerFire.cs
+++ Assets/Scripts/PlayerFire.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerFire : MonoBehaviour
 {
     [Range(0.1f, 3.0f)]
-    public float shootDelay = 3.0f;
+    public float bigShootDelay = 3.0f;
+    [Range(0.1f, 3.0f)]
+    public float topShootDelay = 3.0f;
 
     public GameObject roofGun;
     public GameObject frontGun;
@@ -14,36 +16,61 @@ public class PlayerFire : MonoBehaviour
     public GameObject air2SurfaceMissile;
     public PlayerSound playerAudioScript;
 
-    private bool canShoot = true;
+    private bool bigCanShoot = true;
+    private bool topCanShoot = true;
+    private int topshotCounter;
 
     void Start()
     {
-        
+        topshotCounter = 0;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire1"))
         {
-            Instantiate(air2AirMissile, roofGun.transform);
-            playerAudioScript.WeaponShootUp();
+            if (topCanShoot)
+            {
+                Instantiate(air2AirMissile, roofGun.transform);
+                playerAudioScript.WeaponShootUp();
+                if (topshotCounter == 0) StartCoroutine(TopShootCounterClear());
+                topshotCounter++;
+            }
 
-            if (canShoot) {
+            if (bigCanShoot) {
                 GameObject missile = Instantiate(air2SurfaceMissile, frontGun.transform);
                 Physics2D.IgnoreCollision(missile.GetComponent<Collider2D>(), GetComponent<Collider2D>());
                 playerAudioScript.WeaponShootDown();
-                canShoot = false;
-                StartCoroutine(ShootDelay());
+                bigCanShoot = false;
+                StartCoroutine(BigShootDelay());
             }
             
 
         }
+
+        if ((topshotCounter >= 4) && topCanShoot)
+        {
+            topCanShoot = false;
+            StartCoroutine(TopShootDelay());
+        }
     }
 
-    IEnumerator ShootDelay()
+    IEnumerator BigShootDelay()
     {
-        yield return new WaitForSeconds(shootDelay);
+        yield return new WaitForSeconds(bigShootDelay);
 
-        canShoot = true;
+        bigCanShoot = true;
     }
+    IEnumerator TopShootDelay()
+    {
+        yield return new WaitForSeconds(topShootDelay);
+        topshotCounter = 0;
+        topCanShoot = true;
+    }
+
+    IEnumerator TopShootCounterClear()
+    {
+        yield return new WaitForSeconds(1.0f);
+        if (topshotCounter < 4) topshotCounter = 0;
+    }    
 }
